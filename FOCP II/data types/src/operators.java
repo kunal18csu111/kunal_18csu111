a39d636c55ad918f044da97df7bd01aed1beace8
kunal <dahiyakunal02@gmail.com>
2019-01-11 10:45:02 +0530

    Adding opertaors.java

---
+++ FOCP II/data types/src/operators.java
@@ -0,0 +1,20 @@
+class operators
+{
+	public static void main (String args[]){
+	int z,x,y,s,d,m,u;
+	x=20;
+	y=30;
+	System.out.println("addition operator");
+    z=x+y;
+    System.out.println("value of z is"+z);
+    s=y-x;
+    System.out.println("value after subtraction "+s);
+    d=y/x;
+    System.out.println("value after divison"+d);
+    m=x*y;
+    System.out.println("value after multiplication"+m);
+    u=y%x;
+    System.out.println("value after modulus"+u);
+	}
+
+}
